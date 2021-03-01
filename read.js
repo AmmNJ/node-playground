bb9822c483ba2582ab831c9ae64b80b9e1bd8f2e
Ammersken Nils <nilsammersken@gmail.com>
2021-03-01 16:18:14 +0100

    Update write.js with promises

---
+++ read.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+
+module.exports = function read(path) {
+  fs.promises
+    .readFile(path, { encoding: 'utf-8' })
+    .then(data => console.log(data.split('\n').length))
+}
--- write.js
+++ write.js
@@ -5,13 +5,35 @@ const file = 'tools.json'
 const tool = process.argv[2]
 const score = process.argv[3]
 
-fs.readFile(file, { encoding: 'utf-8' }, (err, data) => {
-  const tools = err ? [] : JSON.parse(data)
+fs.promises
+  .readFile(file, { encoding: 'utf-8' })
+  .then(addToolToJSON)
+  .then(onWriteSuccess)
+  .catch(onError)
+
+function addToolToJSON(data) {
+  const tools = JSON.parse(data)
   tools.push({ name: tool, score: score })
-
-  console.log(tools)
-
-  fs.writeFile(file, JSON.stringify(tools), { encoding: 'utf-8' }, err => {
-    exit(err ? 1 : 0)
+  return fs.promises.writeFile(file, JSON.stringify(tools), {
+    encoding: 'utf-8',
   })
-})
+}
+
+function onWriteSuccess() {
+  exit(0)
+}
+
+function onError(error) {
+  console.error(error)
+}
+// old style with nested callbacks
+// fs.readFile(file, { encoding: 'utf-8' }, (err, data) => {
+//   const tools = err ? [] : JSON.parse(data)
+//   tools.push({ name: tool, score: score })
+
+//   console.log(tools)
+
+//   fs.writeFile(file, JSON.stringify(tools), { encoding: 'utf-8' }, err => {
+//     exit(err ? 1 : 0)
+//   })
+// })
